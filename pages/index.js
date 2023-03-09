124c62785889be5718389453a1c8eba96667d594
AuJung <natthaphong_th@cmu.ac.th>
2023-03-09 19:14:35 +0700

    - fix connection status

--- pages/index.js
+++ pages/index.js
@@ -48,6 +48,7 @@ export default function Home() {
                 }
             });
 
+            client.current.public(topic_LG, "status");
             return () => {
                 setConnection(true);
             }
